35f4fb655c36f4b77e8b350c2fcc713570c02cfb
kohsah <ashok@hariharan.org.in>
2018-04-05 17:52:46 +0530

    Adding exception handlers in saveProfile

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const promisify = require('es6-promisify');
+const serializeError = require('serialize-error');
 
 exports.getProfile = async(req, res) => {
     if(req.query.userName!=undefined){
@@ -20,21 +21,17 @@ exports.getProfile = async(req, res) => {
   }
 
 exports.saveProfile = async(req, res) => {
-	if(req.body.nickName!=undefined && req.body.userName!=undefined){
+  try {
+      if(req.body.nickName!=undefined && req.body.userName!=undefined){
         const user = await User.findOne({ userName: req.body.userName });
         if(user) {
           user.nickName = req.body.nickName;
           await user.save();
-          res.json({ "success": "true", "data": {}});
+          res.json({ "success": "true", "data": {"edited": user.nickName}});
         }else{
           const user = new User({ userName: req.body.userName, nickName: req.body.nickName });
-          user.save(function (err) {
-            if (err){
-            	res.json({ "success": "false", "data": err});
-            }else{
-            	res.json({ "success": "true", "data": {}});
-            }
-          });
+          await user.save();
+          res.json({ "success": "true", "data": {"added": user.nickName}});
         }
     }else if(req.files!=undefined && req.body.userName!=undefined){
         const user = await User.findOne({ userName: req.body.userName });
@@ -43,8 +40,10 @@ exports.saveProfile = async(req, res) => {
           await user.save();
           res.json({ "success": "true", "data": user});
         }else{
-          res.json({ "success": "false", "data": {}});
+          res.json({ "error": "saveProfile", "data": "error while saving image"});
         }
     }
-    return;
+  } catch (err) {
+    res.json({"error": "saveProfile", "data": serializeError(err)});
+  }
   }
